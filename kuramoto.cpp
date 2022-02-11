ed778ae8506b8539e3c23f22a790025926af88d3
MatteoFalcioni <matteo.falk@gmail.com>
2022-02-11 16:07:08 +0100

    skrt

--- kuramoto.cpp
+++ kuramoto.cpp
@@ -8,10 +8,10 @@ static int N;
 void Oscillator::MeanField() {
 
 std::vector<Oscillator> System(N); //insieme degli oscillatori
-
+std::complex<double> X = 0;
 for(int i=0; i<=N; ++i){
-    std::complex<double> X = 0;
     X += System[i].phasor;  //X è la somma di tutti gli esponenziali con fase theta_i
 }
+std::complex<double> Z = (1/N)*X;  //Z è il parametro d'ordine, cioè X rinormalizzato a N. Deve essere espresso come re^i*Psi però
 
 }
