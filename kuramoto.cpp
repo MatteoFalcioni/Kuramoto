64411b0e475836da14f21c74df1276d36eded0cc
MatteoFalcioni <matteo.falk@gmail.com>
2022-02-11 16:38:23 +0100

    lesghere

--- kuramoto.cpp
+++ kuramoto.cpp
@@ -19,7 +19,7 @@ for(int i=0; i<=N; ++i){
 double X = COS/N;
 double Y = SIN/N;
 double r = std::sqrt(X*X + Y*Y);   //possiamo vedere la somma dei fasori come COS/N + i*SIN/N = X + iY. Da qui lo riportiamo in exp
-double psi = std::atan(Y/X);
-std::complex<double> MF = std::polar(r, psi);
+double psi = std::atan(Y/X); 
+std::complex<double> MF = std::polar(r, psi);  //polar costruisce un exp complesso, MF sta per mean field non per Matteo Falcioni
 
 }
