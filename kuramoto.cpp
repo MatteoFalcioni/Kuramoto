488e7c732feb2e7702576474c8d182a2e6407c58
MatteoFalcioni <matteo.falk@gmail.com>
2022-02-11 16:36:08 +0100

    skrt

--- kuramoto.cpp
+++ kuramoto.cpp
@@ -16,10 +16,10 @@ for(int i=0; i<=N; ++i){
     COS += std::cos(System[i].theta_);  //COS è la somma di tutti i coseni di theta_i
     SIN += std::cos(System[i].theta_);  //stesso per SIN
 }
-double r = std::sqrt(COS*COS + SIN*SIN);   //possiamo vedere la somma dei fasori come COS + iSIN = X + iY. Da qui lo riportiamo in exp
-double psi = std::atan(SIN/COS);
+double X = COS/N;
+double Y = SIN/N;
+double r = std::sqrt(X*X + Y*Y);   //possiamo vedere la somma dei fasori come COS/N + i*SIN/N = X + iY. Da qui lo riportiamo in exp
+double psi = std::atan(Y/X);
 std::complex<double> MF = std::polar(r, psi);
-
-std::complex<double> Z;  //Z è il parametro d'ordine, cioè X rinormalizzato a N. Deve essere espresso come re^i*Psi però
 
 }
