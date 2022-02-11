bc24e179d25390f3fa48dcc7ad18ddf26f477075
MatteoFalcioni <matteo.falk@gmail.com>
2022-02-11 16:40:24 +0100

    skere

--- kuramoto.cpp
+++ kuramoto.cpp
@@ -5,7 +5,6 @@ using namespace std::complex_literals;
 static int N;
 
 //definire i parametri di campo medio
-
 void Oscillator::MeanField() {
 
 std::vector<Oscillator> System(N); //insieme degli oscillatori
@@ -19,7 +18,7 @@ for(int i=0; i<=N; ++i){
 double X = COS/N;
 double Y = SIN/N;
 double r = std::sqrt(X*X + Y*Y);   //possiamo vedere la somma dei fasori come COS/N + i*SIN/N = X + iY. Da qui lo riportiamo in exp
-double psi = std::atan(Y/X); 
+double psi = std::atan(Y/X); //non è così semplice calcolare la fase, non so se esiste qualche funzipme che considera i vari casi possibili
 std::complex<double> MF = std::polar(r, psi);  //polar costruisce un exp complesso, MF sta per mean field non per Matteo Falcioni
 
 }
